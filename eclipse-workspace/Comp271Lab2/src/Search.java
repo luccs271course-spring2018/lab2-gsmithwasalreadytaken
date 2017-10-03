58fecc9c6efacd586512640787d97cab0f4bf26a
Grant Smith <Smith9327@Grants-MacBook-Pro.local>
2017-10-03 00:39:10 -0500

    Search finished

--- eclipse-workspace/Comp271Lab2/src/Search.java
+++ eclipse-workspace/Comp271Lab2/src/Search.java
@@ -1,4 +1,103 @@
 
+import java.util.List;
+import java.util.Optional;
+
 public class Search {
 
+  /** Looks for the position of the named team in an array. */
+  public static Optional<Integer> findTeamPosition(final Team[] arr, final String key) {
+    // Gets the array size
+    final int size = arr.length;
+    
+    // Runs through a for loop to check
+    for (int i = 0; i < size; i++) {
+    	
+      // Gets the current item at index and compare name to key
+      if (arr[i].getName().equals(key)) {
+    	  
+        // Return the index of where the item with key is located
+        return Optional.of(i);
+      }
+    }
+    
+    // If it does not exist in the array then return an index of -1
+    return Optional.empty();
+  }
+
+  /** Looks for the position of the named team in a list. */
+  public static Optional<Integer> findTeamPosition(final List<Team> list, final String key) {
+	  final int size = list.size();
+	  for (int i=0; i<size; i++) {
+		  if (list.get(i).getName().equals(key)) {
+			  return Optional.of(i);
+		  }
+    return Optional.empty();
+	  }
+	return null;
+  }
+  
+  /** 
+   * Looks for the position of the poorest team that has at least 
+   * the specified funding level.
+   * @pre arr is sorted
+   * @post arr[result].funding >= minFunding && for all 0 <= i < result : arr[i].funding < minFunding
+   */
+  public static Optional<Integer> findTeamMinFunding(final Team[] arr, final int minFunding) {
+	  
+	  final int size = arr.length;
+	  for (int i=0; i<size; i++) {
+		  if (arr[i].getFunding() >= minFunding) {
+			  return Optional.of(i);
+		  }
+	  }
+	  
+    return Optional.empty();
+  }
+  
+  /** 
+   * Looks for the position of the poorest team that has at least 
+   * the specified funding level. 
+   * Uses binary search: Initially consider the entire index range, 
+   * then repeatedly eliminate the wrong half of the array until 
+   * zero or one items are left.
+   * @pre arr is sorted
+   * @post arr[result].funding >= minFunding && for all 0 <= i < result : arr[i].funding < minFunding
+   */
+  public static Optional<Integer> findTeamMinFundingFast(final Team[] arr, final int minFunding) {
+
+    // Gets the array size
+    final int size = arr.length;
+    
+    // Initially consider the entire index range of the array
+    int low = 0;
+    int high = size - 1;
+    
+    // Keep going as long as there is more than one item to be checked
+    // Eliminate the wrong half of the array
+    // Return current item only if it meets the condition!
+    if (low <= high && arr[low].getFunding() >= minFunding) {
+    	while (low < high) {
+    	            final int mid = (low + high) / 2;
+    	            if (arr[mid].getFunding() >= minFunding) {
+    	            	
+    	                return Optional.of(mid);
+    	                
+    	            }
+    	            
+    	            else if (arr[mid].getFunding() > minFunding) {
+    	                high = mid - 1;
+    	                
+    	            }
+    	            else
+    	                low = mid + 1;
+    	            }
+    	} else {
+    		
+      return Optional.empty();
+      
+    }
+    //I was suggested to either change the type to void or return a "null."
+	System.out.println("Error: Lowest funded team cannot be found.");
+	return null;
+  }
 }
--- eclipse-workspace/Comp271Lab2/src/Team.java
+++ eclipse-workspace/Comp271Lab2/src/Team.java
@@ -1,4 +1,37 @@
-
 public class Team {
+	
+  private String name;
 
+  private String headcoach;
+
+  private int funding;
+
+  public Team(final String name, final String headcoach, final int funding) {
+	  
+    if (name == null) {
+    	
+      throw new IllegalArgumentException("name is null");
+      
+    }
+    
+    this.name = name;
+    
+  }
+
+  public String getName() {
+	  
+    return this.name;
+    
+  }
+  
+  public String getHeadcoach() {
+	  
+    return null;
+    
+  }
+  public int getFunding() {
+	  
+    return -1;
+    
+  }
 }
