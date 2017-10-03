0b49360136c4815415653f7fbe58b27730b992b1
Grant Smith <Smith9327@Grants-MacBook-Pro.local>
2017-10-03 01:11:10 -0500

    Team finished

--- eclipse-workspace/Comp271Lab2/src/Team.java
+++ eclipse-workspace/Comp271Lab2/src/Team.java
@@ -1,37 +1,46 @@
+/** A sports team. */
 public class Team {
 	
+  /** The team's name. */
   private String name;
 
+  /** The team's head coach. */
   private String headcoach;
 
+  /** The team's monetary funding level in thousands of US$. */
   private int funding;
 
+  /** Constructs a new team. */
   public Team(final String name, final String headcoach, final int funding) {
-	  
     if (name == null) {
-    	
       throw new IllegalArgumentException("name is null");
-      
     }
-    
+
+    if (headcoach == null) {
+        throw new IllegalArgumentException("headcoach is null");
+    }
+
+    if (funding < 0) {
+        throw new IllegalArgumentException("funding is null");
+    }
     this.name = name;
-    
+    this.headcoach = headcoach;
+    this.funding = funding;
   }
 
+  /** Returns the team's name. */
   public String getName() {
-	  
     return this.name;
-    
   }
-  
+
+  /** Returns the team's head coach. */
   public String getHeadcoach() {
-	  
-    return null;
-    
+	 return this.headcoach;
   }
+
+  /*
+   * * Returns the team's funding level. */
   public int getFunding() {
-	  
-    return -1;
-    
+	  return this.funding;
   }
 }
